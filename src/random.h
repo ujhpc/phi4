f34a0a9ab75de84f78ba145a3711c534c7373450
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2012-04-26 09:53:12 +0200

    implemented random_array

--- src/random.h
+++ src/random.h
@@ -1,25 +1,39 @@
 #ifndef __RANDOM_H__
 #define __RANDOM_H__
 
-
+#include<cstdlib>
 
 class rand48_array {
   
   
  public:
   
- rand48_array(int i):n_generators_(i) {};
+ rand48_array(int n):n_generators_(n), 
+    seeds_(new unsigned short[3*n_generators_]) {
+
+  };
   
   void gen_seeds(long int);
   void set_seeds(const unsigned  short *seed);
   void get_seeds(unsigned  short *seed) const;
 
-  void next(int i);
-  double value(int i) ;
-  
+  void next(int i) {
+    value_=erand48(seeds_+3*i);
+  };
+
+  double value(int i) const {return value_;};
+
+  double rand(int i) {
+    value_=erand48(seeds_+3*i);
+    return value_;
+  };
+
  private:
   int n_generators_;
-  
+
+  double value_;
+
+  unsigned short *seeds_;
 
 
 };
