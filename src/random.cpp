238628bb00757cde4171e04d34b4e2eb70cf1f69
Adam Strzelecki <ono@java.pl>
2012-05-07 23:59:42 +0200

    Make sure each generator keeps seed in separate cache lines (64 byte blocks).

--- src/random.cpp
+++ src/random.cpp
@@ -6,18 +6,18 @@ const unsigned int two_16=1<<16;
 
 void rand48_array::gen_seeds(long seed) {
   srand48(seed);
-  for(int i=0;i<3*n_generators_;++i) {
+  for(int i=0;i<pitch*n_generators_;++i) {
     seeds_[i]=lrand48()%two_16;
   }
 }
 void rand48_array::set_seeds(const unsigned  short *seed) {
-  for(int i=0;i<3*n_generators_;++i) {
+  for(int i=0;i<pitch*n_generators_;++i) {
     seeds_[i]=seed[i];
   }
 }
 
 void rand48_array::get_seeds(unsigned  short *seed) const {
-  for(int i=0;i<3*n_generators_;++i) {
+  for(int i=0;i<pitch*n_generators_;++i) {
     seed[i]=seeds_[i];
   }
 
