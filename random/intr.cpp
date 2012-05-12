d0435204294bed199d909b031cf1e9f978e8a972
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2012-05-12 14:42:38 +0200

    SSE implementation of tausworth

--- random/intr.cpp
+++ random/intr.cpp
@@ -2,6 +2,8 @@
 
 #include"emmintrin.h"
 
+#include"taus.h"
+
 void taus_step(unsigned &z, int S1,int S2,int S3,unsigned M) {
   unsigned b=(((z<<S1)^z)>>S2);
   z=(((z &M)<<S3)^b);
@@ -12,11 +14,13 @@ void LCG_step(unsigned &z,unsigned A,unsigned C) {
 }
 
 
-void taus_step_SSE2(__m128i &z, int S1,int S2,int S3,unsigned M) {
+inline void  taus_step_SSE2(__m128i &z, int S1,int S2,int S3,unsigned M) {
   const __m128i A = _mm_set_epi32(M,M,M,M);
   __m128i b=_mm_srli_epi32(_mm_xor_si128(_mm_slli_epi32(z,S1),z),S2);
   z=_mm_xor_si128(_mm_slli_epi32(_mm_and_si128(z, A),S3),b);
 }
+
+
 
 
 static inline __m128i muly(const __m128i &a, const __m128i &b)
@@ -30,7 +34,7 @@ static inline __m128i muly(const __m128i &a, const __m128i &b)
 #endif
 }
 
-void LCG_step_SSE2 ( __m128i  &z, unsigned A,unsigned C) {
+inline void LCG_step_SSE2 ( __m128i  &z, unsigned A,unsigned C) {
   const __m128i mm_A = _mm_set_epi32(A,A,A,A);
   const __m128i mm_C = _mm_set_epi32(C,C,C,C);
   z=muly(z,mm_A);
@@ -38,25 +42,62 @@ void LCG_step_SSE2 ( __m128i  &z, unsigned A,unsigned C) {
 }
 
 
+inline  __m128i  irnd_SSE2( __m128i *z) {
+  taus_step_SSE2(z[0],13,19,12,4294967294u);
+  taus_step_SSE2(z[1], 2,25, 4,4294967288u);
+  taus_step_SSE2(z[2], 3,11,17,4294967280u);
+  LCG_step_SSE2(z[3],1664525u,1013904223u);
+  //  return 2.328306436538696e-10*(double)
+  return _mm_xor_si128(_mm_xor_si128(z[0],z[1]),
+		       _mm_xor_si128(z[2],z[3]));
+  
+}
+
+inline  __m128  rnd_sym_SSE2( __m128i z) {
+  const __m128  inv = _mm_set1_ps(2.328306436538696e-10f);
+			       
+  return _mm_mul_ps(_mm_cvtepi32_ps(z),inv);
+}
+
+
+inline  __m128  rnd_SSE2( __m128i z) {
+  const __m128  half = _mm_set1_ps(0.5f);
+  return _mm_add_ps(rnd_sym_SSE2(z),half);
+}
+
 main() {
 
 
   unsigned int z=777;
-  __m128i mm_z = _mm_set_epi32(z,z,z,z);
   
+  unsigned int seed[4]={z,z,z,z};
+  
+  __m128i mm_z = _mm_set_epi32(z,z,z,z);
+  __m128i mm_seed[4];
+  mm_seed[0] =_mm_set_epi32(z,z,z,z);
+  mm_seed[1] =_mm_set_epi32(z,z,z,1232);
+  mm_seed[2] =_mm_set_epi32(z,21,z,z);
+  mm_seed[3] =_mm_set_epi32(z,z,9876,z);
 
-
-  for(int i=0;i<10000000;++i) 
+  double sum=0.0f;
+  for(int i=0;i<1000000;++i) 
     for(int j=0;j<100;j++) {
 #if 0
       taus_step(z,13,19,12,4294967294u);
       taus_step_SSE2(mm_z,13,19,12,4294967294u);
 #else
       //LCG_step(z,1664525u,1013904223u);
-      LCG_step_SSE2(mm_z,1664525u,1013904223u);
+      //LCG_step_SSE2(mm_z,1664525u,1013904223u);
+      __m128 x = rnd_SSE2(irnd_SSE2(mm_seed));
+      float  *t=(float *)&x;
+      sum+=(t[0]+t[1]+t[2]+t[3]);
+
       
+
 #endif
     }
+  
+  fprintf(stdout,"sum = %12.8f\n",sum*0.25e-8);
   
   unsigned int *y=(unsigned int*)&mm_z;
 
--- random/main.cpp
+++ random/main.cpp
@@ -2,71 +2,7 @@
 #include<cstdlib>
 
 
-class taus_array {
-
-public:
-
-  taus_array(int n):n_generators_(n) {
-    seeds_ = new unsigned[4*n_generators_];    
-    gen_seeds(121245);
-  }
-
-inline  double rnd(int i) {
-  taus_step(seeds_[i*4],13,19,12,4294967294u);
-  taus_step(seeds_[i*4+1], 2,25, 4,4294967288u);
-  taus_step(seeds_[i*4+2], 3,11,17,4294967280u);
-  LCG_step(seeds_[i*4+3],1664525u,1013904223u);
-  return 2.328306436538696e-10*(double)(seeds_[i*4]^
-					seeds_[i*4+1]^
-					seeds_[i*4+2]^
-					seeds_[i*4+3]
-					);
-  }
-
-  void gen_seeds(long int);
-  void set_seeds(const unsigned *seed);
-  void get_seeds(unsigned *seed) const;
-  
-private:
-  int n_generators_;
-  unsigned *seeds_;
-
-void taus_step(unsigned &z, int S1,int S2,int S3,unsigned M) {
-    unsigned b=(((z<<S1)^z)>>S2);
-    z=(((z &M)<<S3)^b);
-  }
-  
-void LCG_step(unsigned &z,unsigned A,unsigned C) {
-    z=(A*z+C);
-  }
-
-
-};
-
-void taus_array::gen_seeds(long seed) {
-  srand48(seed);
-  for(int i=0;i<4*n_generators_;++i) {   
-    unsigned r;
-    while( (r=lrand48()) < 128) {};
-    seeds_[i]=r;
-  }
-}
-
-void taus_array::set_seeds(const unsigned   *seed) {
-  for(int i=0;i<4*n_generators_;++i) {
-    seeds_[i]=seed[i];
-  }
-}
-
-void taus_array::get_seeds(unsigned   *seed) const {
-  for(int i=0;i<4*n_generators_;++i) {
-    seed[i]=seeds_[i];
-  }
-}
-
-
-
-
+#include"taus.h"
 
 inline  unsigned taus_step(unsigned &z, int S1,int S2,int S3,unsigned M) {
     unsigned b=(((z<<S1)^z)>>S2);
@@ -119,10 +55,10 @@ main() {
 
   for(int i=0;i<100000000;i++) {
     //    sum+=drand48();
-    //sum1+=taus.rnd(0);
-    //sum2+=taus.rnd(1);
-    sum1+=rand48(seed);
-    sum2+=rand48(seed);
+    sum1+=taus.rnd(0);
+    sum2+=taus.rnd(1);
+    //sum1+=rand48(seed);
+    //sum2+=rand48(seed);
     
     
   }
