3a381767ae382a98de1f413f35b2b302ecff78f4
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2012-06-06 10:10:20 +0200

    ...

--- src/typedefs.h
+++ src/typedefs.h
@@ -3,15 +3,16 @@
 
 #include"random.h"
 
-#define Dim 2
+//#define Dim 2
+#define Dim 3
 #define FLOAT double
 
 typedef Indexer<Dim> Ind;
 
 typedef ScalarFieldAccessor<double,Dim> SFA;
 typedef  Field<FLOAT &, Ind, SFA> ScalarField;
-typedef  cell_partition<Dim,octal_cell<2>,Ind> Partition;
-//typedef  single_partition<Ind> Partition;
+//typedef  cell_partition<Dim,octal_cell<2>,Ind> Partition;
+typedef  single_partition<Ind> Partition;
 
 //inline double RAND(int i) {return rand48_array::generator()->rand(i);}
 //typedef  rand48_array rand_array_t;
