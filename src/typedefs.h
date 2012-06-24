c65aa4e745e05feaca40bb1f18d0771834900788
Adam Strzelecki <ono@java.pl>
2012-06-24 21:15:47 +0200

    Use single source for all 2D & 3D, double and float mode, i.e.:
    make DIM=2 FLOAT=double
    
    make DIM=3 FLOAT=float

--- src/typedefs.h
+++ src/typedefs.h
@@ -1,25 +1,36 @@
 #ifndef __TYPEDEFS_H__
 #define __TYPEDEFS_H__
 
-#include"random.h"
-
-//#define Dim 2
-#define Dim 3
-#define FLOAT double
-
-typedef Indexer<Dim> Ind;
-
-typedef ScalarFieldAccessor<double,Dim> SFA;
-typedef  Field<FLOAT &, Ind, SFA> ScalarField;
-//typedef  cell_partition<Dim,octal_cell<2>,Ind> Partition;
-typedef  single_partition<Ind> Partition;
-
-//inline double RAND(int i) {return rand48_array::generator()->rand(i);}
-//typedef  rand48_array rand_array_t;
-
-inline double RAND(int i) {return taus_array::generator()->rand(i);}
-typedef  taus_array rand_array_t;
-
-//inline double RAND(int i) {return drand48();} 
+#ifndef DIM
+  #define DIM 2
 #endif
- 
+#ifndef FLOAT
+  #define FLOAT double
+#endif
+
+typedef FLOAT Float;
+
+#include "random.h"
+#include "indexer.h"
+#include "field.h"
+#include "partition.h"
+
+typedef Indexer<DIM> Ind;
+typedef ScalarFieldAccessor<Float, DIM> SFA;
+typedef Field<Float &, Ind, SFA> ScalarField;
+
+#if CELL_PARTITION
+typedef cell_partition<DIM, octal_cell<DIM>, Ind> Partition;
+#else
+  typedef single_partition<Ind> Partition;
+#endif
+
+#if USE_RAND48
+  inline Float RAND(int i) { return rand48_array::generator()->rand(i); }
+  typedef rand48_array rand_array_t;
+#else
+  inline Float RAND(int i) { return taus_array::generator()->rand(i); }
+  typedef taus_array rand_array_t;
+#endif
+
+#endif
