886b1632451ef5afd5b9dbad059edf94fe7447ad
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2012-06-28 10:35:46 +0200

    filed inside  Accessor is now reference instead of pointer

--- src/field.h
+++ src/field.h
@@ -81,18 +81,18 @@ public:
   
   class ScalarFieldAccessor {
   public:
-    T get(int i) const {return (*pfield_)[i];}
+    T get(int i) const {return field_[i];}
     T get(int i,int) const {return get(i);}
     T operator()(int i) const  {return get(i);}
     T operator()(int i,int) const  {return get(i);}
     
-    void set(int i, T t) { (*pfield_)[i]=t;}
+    void set(int i, T t) { field_[i]=t;}
     void set(int i,int,T t) { set(t,i);}
     
 
   private:
-    ScalarFieldAccessor(storage_t *p):pfield_(p) {};		       
-    storage_t *pfield_;
+    ScalarFieldAccessor(storage_t &p):field_(p) {};		       
+    storage_t &field_;
     
     friend  class ScalarFieldArray;
   };
@@ -101,7 +101,7 @@ public:
   typedef  ScalarFieldAccessor accessor_t;
 
   accessor_t accessor()  {
-    return ScalarFieldAccessor(&field_);
+    return ScalarFieldAccessor(field_);
   }
 
 
@@ -116,6 +116,7 @@ class ScalarField {
  public:
   typedef  typename A::scalar_t scalar_t;
   typedef  Indexer indexer_t;
+  typedef typename A::accessor_t accessor_t;
 
  ScalarField(int n = indexer_t::n_sites()):field_(n), 
     accessor_(field_.accessor()) {
@@ -128,10 +129,12 @@ class ScalarField {
     
   void set(int i,     scalar_t t) { accessor_.set(i,t);}
   void set(int i,int, scalar_t t) { set(t,i);}
-
+  
+  accessor_t accessor() {return accessor_;}
+  
  private:
   A field_;
-  typename A::accessor_t accessor_;
+  accessor_t accessor_;
 };
 
 
