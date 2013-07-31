342a8ca1bb14e91889d44241982ca2cb6d47883d
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2013-08-01 00:54:21 +0200

    Merge cache into main phi4

--- src/phi4.cpp
+++ src/phi4.cpp
@@ -6,6 +6,11 @@ using namespace std;
 #include <cmath>
 #include <popt.h>
 
+#ifdef __LINUX__
+#include <time.h>
+#include "linux_time.h"
+#endif
+
 template <typename T> int poptType();
 template <> int poptType<float>() { return POPT_ARG_FLOAT; }
 template <> int poptType<double>() { return POPT_ARG_DOUBLE; }
@@ -45,6 +50,23 @@ int main(int argc, const char* argv[]) {
   n_y = 32;
   int n_z = 32;
 #endif
+
+#ifdef CACHE
+  int block_dim[DIM];
+  int block_sweeps = 8;
+  int b_x = 16;
+  int b_y = 16;
+#endif
+#if DIM >= 3
+  b_x = 8;
+  b_y = 8;
+#ifdef CACHE
+  int b_z = 8;
+#endif
+#else
+  const int block_sweeps = 1;
+#endif
+
 #ifdef _OPENMP
   int threads = 0;
 #endif
@@ -66,6 +88,14 @@ int main(int argc, const char* argv[]) {
     { "n-y", 0, POPT_ARG_INT, &n_y, 0, "y size" },
 #if DIM >= 3
     { "n-z", 0, POPT_ARG_INT, &n_z, 0, "z size" },
+#endif
+#ifdef CACHE
+    { "b-x", 0, POPT_ARG_INT, &b_x, 0, "block x size" },
+    { "b-y", 0, POPT_ARG_INT, &b_y, 0, "block y size" },
+#if DIM >= 3
+    { "b-z", 0, POPT_ARG_INT, &b_z, 0, "block z size" },
+#endif
+    { "block-sweeps", 0, POPT_ARG_INT, &block_sweeps, 0 },
 #endif
     { "term", 't', POPT_ARG_INT, &n_term, 0, "number of termalisation sweeps" },
     { "sweep", 'n', POPT_ARG_INT, &n_prod, 0, "number of measurments sweeps" },
@@ -100,13 +130,23 @@ int main(int argc, const char* argv[]) {
   dim[2] = n_z;
 #endif
 
+#ifdef CACHE
+  block_dim[0] = b_x;
+  block_dim[1] = b_y;
+#if DIM >= 3
+  block_dim[2] = b_z;
+#endif
+#endif
+
   std::string mag_file_name("mag.");
   mag_file_name += std::string(tag);
 
   Ind::init(dim);
+#ifdef CACHE
+  BlockType::init(block_dim);
+#endif
 
   Field phi_field(Ind::n_sites());
-
   srand48(seed);
 
 #ifdef _OPENMP
@@ -121,7 +161,6 @@ int main(int argc, const char* argv[]) {
   int n_threads = 1;
   std::cerr << "# OpenMP No\n";
 #endif
-
   rand_array_t::init(n_threads, seed);
 
   for (int i = 0; i < Ind::n_sites(); i++) {
@@ -131,19 +170,38 @@ int main(int argc, const char* argv[]) {
   /*
    * termalisation loop
    */
-
   long int accepted = 0;
-
   Partition partition;
 
+#ifdef __LINUX__
+  LinuxTimer timer(CLOCK_MONOTONIC);
+  if (timer)
+    timer.print_res();
+  timer.start();
+#endif
+
   for (int sweep = 0; sweep < n_term; sweep++) {
+#ifdef CACHE
+    accepted += make_sweep(phi_field, pars, block_sweeps);
+#else
     accepted += make_sweep(phi_field, pars, partition);
+#endif
   }
+  timer.stop();
+
+#ifdef __LINUX__
+  fprintf(stdout, "termalisation took %lld ns\n", timer.ellapsed_time());
+  double fnano_sec = (double)timer.ellapsed_time();
+  fprintf(stderr,
+          "that makes %lf ns per update\n",
+          fnano_sec / (block_sweeps * N_HIT * (double)Ind::n_sites() * n_term));
+#endif
 
   if (n_term > 0)
     fprintf(stderr,
             "acceptance %f\n",
-            ((double)accepted) / (N_HIT * (double)Ind::n_sites() * n_term));
+            ((double)accepted) /
+                (block_sweeps * N_HIT * (double)Ind::n_sites() * n_term));
 
   MagnetisationMeasurer<Field::n_components> magnetisation;
   accepted = 0;
@@ -160,15 +218,14 @@ int main(int argc, const char* argv[]) {
   }
   int n_meas = 0;
   for (int sweep = 0; sweep < n_prod; sweep++) {
-
+#ifdef CACHE
+    accepted += make_sweep(phi_field, pars, block_sweeps);
+#else
     accepted += make_sweep(phi_field, pars, partition);
-
+#endif
     if ((sweep + 1) % meas_freq == 0) {
-
       magnetisation.measure(phi_field);
-
       if ((magnetisation.n_meas()) % write_out == 0) {
-
         fprintf(fmag,
                 "%.12g  %.16g %.16g %.16g\n",
                 magnetisation.phi2() / Ind::n_sites(),
@@ -179,7 +236,6 @@ int main(int argc, const char* argv[]) {
       }
       n_meas++;
     }
-
     if (n_meas > 0 && n_meas % 100 == 0)
       fflush(fmag);
   }
@@ -187,8 +243,9 @@ int main(int argc, const char* argv[]) {
   if (n_prod > 0)
     fprintf(stderr,
             "acceptance %lf\n",
-            ((double)accepted) /
-                ((double)N_HIT * (double)Ind::n_sites() * (double)n_prod));
-
+            ((double)accepted) / (block_sweeps * (double)N_HIT *
+                                  (double)Ind::n_sites() * (double)n_prod));
+#ifndef CACHE
   Ind::clean();
+#endif
 }
