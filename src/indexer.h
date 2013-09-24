85898498167db7e85be974810131b0bc07dd36f0
Adam Strzelecki <ono@java.pl>
2013-09-24 17:59:13 +0200

    Refactor in-memory indexer too

--- src/indexer.h
+++ src/indexer.h
@@ -4,46 +4,19 @@
 
 #include "iterator.h"
 
-template <int Dim> class Indexer {
-
-  typedef int coordinate_t;
-
+template <int Dim, typename Site = int, typename Direction = int>
+class Indexer {
  public:
-  typedef LatticeIterator<Dim> iterator;
   enum {
     D = Dim
   };
+  typedef Site coordinate_t[Dim];
+  typedef Site site_t;
+  typedef Direction direction_t;
 
-  static coordinate_t site(const coordinate_t* coord) {
-    int s = 0;
+  typedef LatticeIterator<Dim> iterator;
 
-    for (int i = 0; i < Dim; i++)
-      s += vol(i) * coord[i];
-
-    return s;
-  }
-
-  static coordinate_t site_safe(const coordinate_t* coord) {
-    int c[D];
-    for (int i = 0; i < D; ++i) {
-      c[i] = coord[i];
-      if (c[i] >= dim(i))
-        c[i] -= dim(i);
-      if (c[i] < 0)
-        c[i] += dim(i);
-    }
-    return site(c);
-  }
-
-  static coordinate_t index(coordinate_t site, coordinate_t dir) {
-    return dir * vols_[Dim] + site;
-  }
-
-  static coordinate_t index(const coordinate_t* coord, coordinate_t dir) {
-    return dir * vols_[Dim] + site(coord);
-  }
-
-  static void init(const coordinate_t* dim) {
+  static void init(const coordinate_t dim) {
     vols_[0] = 1;
 
     for (int i = 0; i < Dim; i++) {
@@ -56,6 +29,40 @@ template <int Dim> class Indexer {
     init_arrays();
   }
 
+  static void clean() {
+    delete[] up_;
+    delete[] dn_;
+  }
+
+  static site_t site(const coordinate_t coord) {
+    int s = 0;
+
+    for (int i = 0; i < Dim; i++)
+      s += vol(i) * coord[i];
+
+    return s;
+  }
+
+  static site_t site_safe(const coordinate_t coord) {
+    int c[D];
+    for (int i = 0; i < D; ++i) {
+      c[i] = coord[i];
+      if (c[i] >= dim(i))
+        c[i] -= dim(i);
+      if (c[i] < 0)
+        c[i] += dim(i);
+    }
+    return site(c);
+  }
+
+  static site_t index(site_t site, direction_t dir) {
+    return dir * vols_[Dim] + site;
+  }
+
+  static site_t index(const coordinate_t coord, direction_t dir) {
+    return dir * vols_[Dim] + site(coord);
+  }
+
   static int dim(int i) { return dims_[i]; }
   static int dim() { return Dim; }
   static const int* dims() { return &dims_[0]; }
@@ -66,18 +73,19 @@ template <int Dim> class Indexer {
   static LatticeIterator<Dim> begin() { return LatticeIterator<Dim>(dims_); }
   static LatticeIterator<Dim> end() { return LatticeIterator<Dim>(); }
 
-  static coordinate_t up(coordinate_t site, coordinate_t dir) {
+  static site_t up(site_t site, direction_t dir) {
     return up_[index(site, dir)];
   }
 
-  static coordinate_t dn(coordinate_t site, coordinate_t dir) {
+  static site_t dn(site_t site, direction_t dir) {
     return dn_[index(site, dir)];
   }
 
+ private:
   static void init_arrays() {
 
-    up_ = new coordinate_t[n_links_];
-    dn_ = new coordinate_t[n_links_];
+    up_ = new site_t[n_links_];
+    dn_ = new site_t[n_links_];
 
     for (iterator it = begin(); it != end(); ++it) {
 
@@ -104,26 +112,31 @@ template <int Dim> class Indexer {
     }
   }
 
-  static void clean() {
-    delete[] up_;
-    delete[] dn_;
-  }
+  static site_t n_links_;
 
- private:
-  static coordinate_t n_links_;
+  static site_t dims_[Dim];
+  static site_t vols_[Dim + 1];
 
-  static coordinate_t dims_[Dim];
-  static coordinate_t vols_[Dim + 1];
-
-  static coordinate_t* up_;
-  static coordinate_t* dn_;
+  static site_t* up_;
+  static site_t* dn_;
 };
 
-template <int Dim> typename Indexer<Dim>::coordinate_t Indexer<Dim>::n_links_;
+template <int Dim, typename Site, typename Direction>
+typename Indexer<Dim, Site, Direction>::site_t
+    Indexer<Dim, Site, Direction>::n_links_;
 
-template <int Dim> typename Indexer<Dim>::coordinate_t Indexer<Dim>::dims_[Dim];
-template <int Dim>
-typename Indexer<Dim>::coordinate_t Indexer<Dim>::vols_[Dim + 1];
+template <int Dim, typename Site, typename Direction>
+typename Indexer<Dim, Site, Direction>::site_t
+    Indexer<Dim, Site, Direction>::dims_[Dim];
 
-template <int Dim> typename Indexer<Dim>::coordinate_t* Indexer<Dim>::up_;
-template <int Dim> typename Indexer<Dim>::coordinate_t* Indexer<Dim>::dn_;
+template <int Dim, typename Site, typename Direction>
+typename Indexer<Dim, Site, Direction>::site_t
+    Indexer<Dim, Site, Direction>::vols_[Dim + 1];
+
+template <int Dim, typename Site, typename Direction>
+typename Indexer<Dim, Site, Direction>::site_t*
+    Indexer<Dim, Site, Direction>::up_;
+
+template <int Dim, typename Site, typename Direction>
+typename Indexer<Dim, Site, Direction>::site_t*
+    Indexer<Dim, Site, Direction>::dn_;
