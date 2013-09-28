641476622ec827cbdfcc0b4efb87c54a87c9ad3b
Adam Strzelecki <ono@java.pl>
2013-09-28 12:56:10 +0200

    No need for libsvml fix on newer ICC

--- src/libsvml_wrapper_linux.cpp
+++ src/libsvml_wrapper_linux.cpp
@@ -1,6 +1,6 @@
 // NOTE: This file is only for Linux and it is
 // temporary solution for missing symbols when
-// linking to SVML Intel library.
+// linking to old SVML Intel library (ICC<13).
 
 extern "C" {
 #if !defined(__INTEL_COMPILER) && defined(__linux__)
@@ -9,26 +9,29 @@ extern "C" {
 // and requires some built-in function generated
 // by ICC linker, if we are not using ICC let's
 // pretend we have valid function.
-int __intel_cpu_indicator = 0;
-// CPU dispatcher function
-void __intel_cpu_indicator_init(void) {
-  // CPU_AVX
-  // __intel_cpu_indicator = 0x20000;
-  // CPU_SSE42
-  __intel_cpu_indicator = 0x8000;
-  // CPU_SSE4
-  // __intel_cpu_indicator = 0x2000;
-  // CPU_SSSE3
-  // __intel_cpu_indicator = 0x1000;
-  // CPU_SSE3
-  __intel_cpu_indicator = 0x800;
-  // CPU_SSE2 && !(CPU_SSE2_IS_SLOW) )
-  // _intel_cpu_indicator = 0x200;
-  // CPU_SSE
-  // __intel_cpu_indicator = 0x80;
-  // CPU_MMX2
-  // __intel_cpu_indicator = 8;
-  // __intel_cpu_indicator = 1;
-}
+#if defined __AVX2__
+int __intel_cpu_indicator = 0x40000;
+#elif defined __AVX__
+int __intel_cpu_indicator = 0x20000;
+#elif defined __SSE4_2__
+int __intel_cpu_indicator = 0x8000;
+#elif defined __SSE4_1__
+int __intel_cpu_indicator = 0x2000;
+#elif defined __SSSE3__
+int __intel_cpu_indicator = 0x1000;
+#elif defined __SSE3__
+int __intel_cpu_indicator = 0x800;
+#elif defined __SSE2_MATH__  // !(CPU_SSE2_IS_SLOW)
+int _intel_cpu_indicator = 0x200;
+#elif defined __SSE__
+int __intel_cpu_indicator = 0x80;
+#elif defined __MMX__
+int __intel_cpu_indicator = 8;
+#else
+int __intel_cpu_indicator = 1;
+#endif
+
+// CPU dispatcher function (dummy)
+void __intel_cpu_indicator_init(void) {}
 #endif
 }
